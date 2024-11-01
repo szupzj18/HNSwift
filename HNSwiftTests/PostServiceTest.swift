0fdd0dfe36a8a8f3fb5c4549b7223c8b321a28f0
ChrisPan <szupzj18@outlook.com>
2024-11-01 15:55:20 +0800

    add test for request post ids

--- HNSwiftTests/PostServiceTest.swift
+++ HNSwiftTests/PostServiceTest.swift
@@ -10,5 +10,20 @@ final class HNPostServiceTests: XCTestCase {
     
     override class func tearDown() {
         URLProtocol.unregisterClass(MockURLProtocol.self)
+        super.tearDown()
+    }
+    
+    func testFetchTopStoryIDs() async throws {
+        let mockData = "[1, 2, 3]".data(using: .utf8)
+        guard let mockData = mockData else {
+            return
+        }
+        MockURLProtocol.requestHandler = { request in
+            return (HTTPURLResponse(), mockData)
+        }
+        
+        let service = PostService()
+        let postIDs = try await service.fetchTopStoryIDs()
+        XCTAssertEqual(postIDs, [1, 2, 3])
     }
 }
